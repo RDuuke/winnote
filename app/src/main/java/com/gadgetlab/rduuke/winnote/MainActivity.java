12985462a084539d7227a87e17d655e1e8b97921
RDuuke <juuanduuke@gmail.com>
2018-12-10 16:39:42 -0500

    second commit

---
+++ app/src/main/java/com/gadgetlab/rduuke/winnote/MainActivity.java
@@ -0,0 +1,17 @@
+package com.gadgetlab.rduuke.winnote;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivy";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
